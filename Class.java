ecd177a13398e298b70b4b7627c045a161682daa
igwevsiggy15 <53847461+igwevsiggy15@users.noreply.github.com>
2019-08-14 14:52:11 -0400

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,10 @@
+package Assignment7_1;
+
+class Class 
+{ 
+  public static void main(String args[]) 
+  { 
+    String str7 = new StringBuilder().append(1234).toString(); 
+    System.out.println("String str7 = " + str7); 
+  } 
+}
